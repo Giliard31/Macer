097bd3f15c813e2f63a625a8919d7df6c555453f
Giliard31 <113076201+Giliard31@users.noreply.github.com>
2026-06-24 15:44:27 -0300

    Update service worker cache version and assets

--- sw.js
+++ sw.js
@@ -1,14 +1,29 @@
-const CACHE_NAME = 'marcenaria-v1';
+const CACHE_NAME = 'marcenaria-v4';
 const ASSETS = [
-  'index.html',
-  'manifest.json'
+  './',
+  './index.html',
+  './manifest.json'
 ];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS);
-    })
+    }).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
   );
 });
 
